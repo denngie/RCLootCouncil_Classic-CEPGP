2896eeb211d24c695270a88983e7d784d7a20714
Dennis Israelsson <github@mdh.nu>
2023-01-31 22:13:34 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# RCLootCouncil_Classic-EPGP
-An extension to RCLC that adds CEPGP support
+# RCLootCouncil Classic-CEPGP
+A module adding CEPGP support for RCLootCouncil Classic
+
+How does it work? It inserts an extra column, showing the PR value of each member, in the voting frame. When you assign an item the calculcated GP value is added to the winner. The module reuses CEPGP's own functions for these calculations. The button/response text is forwarded to CEPGP and visible in CEPGP traffic. All loot will be visible in both RC LC history and CEPGP traffic.
+
+## How to use
+When you start a master looter session answer yes to use RC LC to handle loot and no to CEPGP. Master looter must have this addon, everyone in your loot council are optional depending on if they wanna see the PR column. Raid members only needs the standard RC LC Classic addon.
+
+## Known limitations
+* Reawarding an item to change the winner does not subtract the GP award to the previous winner
+* Discount is not supported
+* If RC LC test mode is used it will not run the CEPGP add GP function
+* If one of the following responses is used it will not run the CEPGP add GP function:
+..* Disenchant
+..* Banking
+..* Free
+..* Pass
+..* Autopass
+* If a button/response contains the word free the GP value is reduced to zero
+
+## Credits
+Shoutout to bagstone and his addon RCLootCouncil_PlusOne which was a big inspiration for this.
---
+++ votingFrameCEPGP.lua
@@ -0,0 +1,69 @@
+local addon = LibStub("AceAddon-3.0"):GetAddon("RCLootCouncil_Classic")
+local RCVotingFrame = addon:GetModule("RCVotingFrame")
+local RCVFCEPGP = addon:NewModule("RCVFCEPGP", "AceEvent-3.0")
+
+local table = table
+
+
+function RCVFCEPGP:OnInitialize()
+	if not RCVotingFrame.scrollCols then -- RCVotingFrame hasn't been initialized.
+		return self:ScheduleTimer("OnInitialize", 0.5)
+	end
+    self:UpdateColumns()
+	self.initialize = true
+end
+
+function RCVFCEPGP:GetScrollColIndexFromName(colName)
+    for i, v in ipairs(RCVotingFrame.scrollCols) do
+        if v.colName == colName then
+            return i
+        end
+    end
+end
+
+function RCVFCEPGP:UpdateColumns()
+    local CEPGP =
+    { name = "PR", DoCellUpdate = self.SetCellPR, colName = "PR", sortnext = self:GetScrollColIndexFromName("response"), width = 30, align = "CENTER", defaultsort = "asc" }
+	table.insert(RCVotingFrame.scrollCols, CEPGP)
+
+    self:ResponseSortNext()
+
+    if RCVotingFrame:GetFrame() then
+        RCVotingFrame:GetFrame().UpdateSt()
+    end
+end
+
+function RCVFCEPGP:ResponseSortNext()
+    local responseIdx = self:GetScrollColIndexFromName("response")
+    local prIdx = self:GetScrollColIndexFromName("PR")
+
+    if responseIdx then
+        RCVotingFrame.scrollCols[responseIdx].sortnext = prIdx
+    end
+end
+
+function RCVFCEPGP.SetCellPR(rowFrame, frame, data, cols, row, realrow, column, fShow, table, ...)
+	local name = Ambiguate(data[realrow].name, "short")
+	local index = CEPGP_getIndex()
+	local EP, GP = CEPGP_getEPGP(name, index);
+	local PR = math.floor((EP/GP)*100)/100
+
+	frame.text:SetText(PR)
+	data[realrow].cols[column].value = PR or -1
+end
+
+function RCVFCEPGP:RCMLAwardSuccess(message, session, winner, status, link, responseText)
+	local player = Ambiguate(winner, "short")
+	local itemID = CEPGP_getItemID(link)
+	local ignore_responses = { Disenchant=true, Banking=true, Free=true, Pass=true, Autopass=true }
+
+	if not ignore_responses[responseText] and not addon.testMode then
+		local amount = CEPGP_calcGP(_, 1, itemID)
+		if responseText:find("free") then
+			amount = 0
+		end
+
+		CEPGP_addGP(player, amount, itemID, link, nil, responseText)
+	end
+end
+RCVFCEPGP:RegisterMessage("RCMLAwardSuccess")
